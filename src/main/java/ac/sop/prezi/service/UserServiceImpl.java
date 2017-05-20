b2bd9abdf38057aacee051140ba4e4775b498fd1
DIuliana <danea.iuliana@gmail.com>
2017-05-20 17:09:45 +0300

    error handling for api

--- src/main/java/ac/sop/prezi/service/UserServiceImpl.java
+++ src/main/java/ac/sop/prezi/service/UserServiceImpl.java
@@ -15,33 +15,41 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	UserRepository userRepository;
 
-
 	@Override
 	public void save(User user) {
-	
+
 		userRepository.save(user);
 
 	}
 
 	@Override
 	public List<User> findAll() {
-		
+
 		return userRepository.findAll();
 	}
 
 	@Override
 	public User findById(Long id) {
-		
+
 		return userRepository.findById(id);
 	}
 
-
 	@Override
 	public void custom() {
-		
+
 		userRepository.customMehod();
 
 	}
 
+	@Override
+	public User findByUserNameAndPassword(String userName, String pass) throws UserNotFoundExcecption {
+
+		User user = userRepository.findByUserNameAndPassword(userName, pass);
+		if (user == null) {
+			throw new UserNotFoundExcecption();
+		} else {
+			return user;
+		}
+	}
 
 }
--- src/main/java/ac/sop/prezi/web/controllers/HomeController.java
+++ src/main/java/ac/sop/prezi/web/controllers/HomeController.java
@@ -1,15 +1,36 @@
 package ac.sop.prezi.web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
+import ac.sop.prezi.persist.entities.User;
+import ac.sop.prezi.service.UserNotFoundExcecption;
+import ac.sop.prezi.service.interfaces.AuthService;
+
+@Controller
+@RequestMapping("/auth")
 public class HomeController {
-	
-	@RequestMapping("/")
-	public String home(){
-		
-		return "Alex este o capra mica!";
-	}
 
+	@Autowired
+	AuthService authService;
+
+	@RequestMapping(value = "/{username}/{password}", method = RequestMethod.GET)
+	public ResponseEntity<?> auth(@PathVariable String username, @PathVariable String password) {
+
+		try {
+			User user = authService.auth(username, password);
+			return new ResponseEntity<>(user, HttpStatus.OK);
+		} catch (UserNotFoundExcecption exception) {
+
+			String errorMessage;
+			errorMessage = exception + " <== error";
+			return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+		}
+
+	}
 }
